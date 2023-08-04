5f3bff3f5badba3a77d878d61ae1310972bad577
radovanmatula <209933@vutbr.cz>
2023-08-04 14:59:35 +0200

    View_watchlist now triggers a new window, random_film_suggestion is a popup which disappears after couple of seconds

--- app.py
+++ app.py
@@ -10,114 +10,131 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 
-###
-from kivy.config import Config
-Config.set('graphics', 'resizable', True) 
-#Config.set('graphics', 'width', '350')
-#Config.set('graphics', 'height', '130')
-Config.write()
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-#FG imports
+from kivy.core.window import Window
+
+from kivy.clock import Clock
+
+
+#SM imports
 from watchlist import Watchlist
 
 ####
 
-class WatchlistEntry(GridLayout):
+class MainScreen(Screen):
 
     def __init__(self, **kwargs):
-        super(WatchlistEntry, self).__init__(**kwargs)
+        super(MainScreen, self).__init__(**kwargs)
        
-        # Initialise FG
+        # Initialise SparkleMotion
         self.SMWatchlist = Watchlist()
         
-
         #the whole window is defaultly just one column so we can vary the number of columns on different rows
-        self.cols = 1
-        self.add_widget(Label(text='Welcome to SparkleMotion'))
+        lay_out = GridLayout(cols=1)
+        lay_out.add_widget(Label(text='Welcome to SparkleMotion'))
 
-        self.inner = GridLayout(cols=2) # the app layout will be split into two columns
-        self.add_widget(self.inner)
+        lay_in = GridLayout(cols=2) # the app layout will be split into two columns
+        lay_out.add_widget(lay_in)
         
         self.knew_film = TextInput(multiline=True, font_size=30)
-        self.inner.add_widget(self.knew_film)
+        lay_in.add_widget(self.knew_film)
 
         self.add_button = Button(text='Add Film')
         self.add_button.bind(on_press=self.kadd_to_watchlist)
-        self.inner.add_widget(self.add_button)
+        lay_in.add_widget(self.add_button)
 
         self.random_button = Button(text='Random Pick')
         self.random_button.bind(on_press=self.krandom_film_suggestion)
-        self.add_widget(self.random_button)
+        lay_out.add_widget(self.random_button)
 
         self.view_button = Button(text='View Watchlist')
-        self.view_button.bind(on_press=self.kview_watchlist)
-        self.add_widget(self.view_button)
-       
+        self.view_button.bind(on_press=self.open_watchlist)
+        lay_out.add_widget(self.view_button)
+        
+        # add the`main` layout as a widget to visualise the whole thing
+        self.add_widget(lay_out)
         
     # Actions in the app
     def kadd_to_watchlist(self, instance):
 
         new_film = self.knew_film.text
         self.SMWatchlist.add_to_watchlist(new_film)
-
+    
+    
     def krandom_film_suggestion(self, instance):
-        
+
         random_pick = self.SMWatchlist.random_film_suggestion()
-        
-        layout = GridLayout(cols = 1, padding = 10)
+        random_popup = RandomSuggestionPopup(title = 'Random Film Suggestion', content=Label(text=random_pick, font_size=30))
+        random_popup.open()
 
-        popupText = Label(text = random_pick)
-        closeButton = Button(text = "Close")
+    
+    def open_watchlist(self, *args):
+        Window.size = (800, 500)       
+        self.manager.current = 'watchlist'
 
-        layout.add_widget(popupText)
-        layout.add_widget(closeButton)
 
-        # Instantiate the modal popup and display
-        popup = Popup(
-                title ='Random Film Suggestion',
-                content = layout,
-                size=(200,100)
-                )
-        popup.open()
+#Screen for `view_watchlist`
+class WatchlistScreen(Screen):
 
-        # Attach close button press with popup.dismiss action
-        closeButton.bind(on_press = popup.dismiss)
+    def __init__(self, **kwargs):
+        super(WatchlistScreen, self).__init__(**kwargs)
 
-    # Temp function
-    def kview_watchlist(self, instance):
+        # Initialise SparkleMotion
+        self.SMWatchlist = Watchlist()
+    
+        lay_out = GridLayout(cols=1)
 
-        Config.set('graphics', 'width', '750')
-        Config.set('graphics', 'height', '300')
-        Config.write()
-
+        # watchlist text now provided via `view_watchlist` method but might be better if we format it here in kivy
         text = self.SMWatchlist.view_watchlist()
-        
-        layout = GridLayout(cols = 1, padding = 10)
 
-        popupText = Label(text = text)
-        closeButton = Button(text = "Close", size=(700,100))
+        lay_out.add_widget(Label(text = text))
 
-        layout.add_widget(popupText)
-        layout.add_widget(closeButton)
 
-        # Instantiate the modal popup and display
-        popup = Popup(
-                title ='Your Watchlist',
-                content = layout,
-                size=(700,700)
-                )
-        popup.open()
+        # close button
+        close_button = Button(text='X', size_hint=(None, None), size=(50, 50), pos_hint={'top': 0, 'right': 0})
+        close_button.bind(on_press=self.close_watchlist)
+        lay_out.add_widget(close_button)
 
-        # Attach close button press with popup.dismiss action
-        closeButton.bind(on_press = popup.dismiss)
+        # Visualise layout
+        self.add_widget(lay_out)
 
-    
-    
 
+    # close WachlistScreen and return to MainScreen
+    def close_watchlist(self, *args):
+        Window.size = (350, 200)
+        self.manager.current = 'main'
+
+
+# Random Suggestion
+class RandomSuggestionPopup(Popup):
+
+    def __init__(self, **kwargs):
+        super(RandomSuggestionPopup, self).__init__(**kwargs)
+
+        self.auto_dismiss = False
+        self.dismiss_after = 3  # seconds until the popup disappears
+        Clock.schedule_once(self.dismiss_random_popup, self.dismiss_after)
+
+
+    def dismiss_random_popup(self, dt):
+        self.dismiss()
+
+
+####
 class SparkleMotion(App):
 
     def build(self):
-        return WatchlistEntry()
+        manager = ScreenManager()
+        
+        main_screen = MainScreen(name='main')
+        manager.add_widget(main_screen)
+
+        watchlist_screen = WatchlistScreen(name='watchlist')
+        manager.add_widget(watchlist_screen)
+        
+        return manager
+
 
 # Run it
 if __name__ == '__main__':
