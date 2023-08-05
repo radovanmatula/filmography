9f3b078187018ba199aaf36806741ed9e1ebdc55
radovanmatula <209933@vutbr.cz>
2023-08-05 19:38:40 +0200

    Watchlist now print into a nice scrollable table

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ from kivy.core.window import Window
 
 from kivy.clock import Clock
 
+from kivy.uix.scrollview import ScrollView
+
 
 #SM imports
 from watchlist import Watchlist
+SMWatchlist = Watchlist()
 
 ####
 
@@ -26,9 +29,6 @@ class MainScreen(Screen):
 
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-       
-        # Initialise SparkleMotion
-        self.SMWatchlist = Watchlist()
         
         #the whole window is defaultly just one column so we can vary the number of columns on different rows
         lay_out = GridLayout(cols=1)
@@ -59,24 +59,24 @@ class MainScreen(Screen):
     def kadd_to_watchlist(self, instance):
 
         new_film = self.knew_film.text
-        self.SMWatchlist.add_to_watchlist(new_film)
+        SMWatchlist.add_to_watchlist(new_film)
     
     
     def krandom_film_suggestion(self, instance):
 
-        random_pick = self.SMWatchlist.random_film_suggestion()
+        random_pick = SMWatchlist.random_film_suggestion()[0]
         random_popup = DismissPopup(
                 title = 'Random Film Suggestion', 
                 content=Label(text=random_pick, 
                               font_size=20),
                 size_hint = (0.6, 0.4),
-                pos_hint = {'center_x': 0.5, 'center_y': 0.5}
-                )
+                pos_hint = {'center_x': 0.5, 'center_y': 0.5})
+
         random_popup.open()
 
     
     def open_watchlist(self, *args):
-        Window.size = (800, 500)       
+        Window.size = (800, 400)       
         self.manager.current = 'watchlist'
 
 
@@ -86,29 +86,47 @@ class WatchlistScreen(Screen):
     def __init__(self, **kwargs):
         super(WatchlistScreen, self).__init__(**kwargs)
 
-        # Initialise SparkleMotion
-        self.SMWatchlist = Watchlist()
-    
         lay_out = GridLayout(cols=1)
+        
+        # Create watchlist table
+        film_infos = ['name', 'director', 'runtime', 'year_of_release']
+        table_layout = GridLayout(cols=len(film_infos), size_hint_y=None)
 
-        # watchlist text now provided via `view_watchlist` method but might be better if we format it here in kivy
-        text = self.SMWatchlist.view_watchlist()
+        # Create the header
+        for info in film_infos:
+            table_layout.add_widget(Label(text=info.upper(), bold=True))
 
-        lay_out.add_widget(Label(text = text))
+        watchlist = SMWatchlist._watchlist_dictionary
 
+        for film, film_info in watchlist.items():
+            for info in film_infos:
+                table_layout.add_widget(Label(text=str(film_info[info])))
+
+        # Make the watchlist printable 
+        # Calculate the height of the table based on the number of entries
+        table_height = len(watchlist.keys()) * 35  # Assuming each row is 50 pixels high
+
+        # Set the height of the table layout
+        table_layout.height = table_height
+        table_layout.bind(minimum_height=table_layout.setter('height'))
+
+        # Create the ScrollView and add the table layout to it
+        scroll_view = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        scroll_view.add_widget(table_layout)
+
+        lay_out.add_widget(scroll_view)
 
         # close button
-        close_button = Button(text='X', size_hint=(None, None), size=(50, 50), pos_hint={'top': 0, 'right': 0})
+        close_button = Button(text='<--', size_hint=(None, None), size=(50, 50), pos_hint={'top': 0, 'right': 0})
         close_button.bind(on_press=self.close_watchlist)
         lay_out.add_widget(close_button)
 
         # Visualise layout
         self.add_widget(lay_out)
 
-
     # close WachlistScreen and return to MainScreen
     def close_watchlist(self, *args):
-        Window.size = (350, 200)
+        Window.size = (450, 250)
         self.manager.current = 'main'
 
 
@@ -119,7 +137,7 @@ class DismissPopup(Popup):
         super(DismissPopup, self).__init__(**kwargs)
 
         self.auto_dismiss = False
-        self.dismiss_after = 3  # seconds until the popup disappears
+        self.dismiss_after = 2  # seconds until the popup disappears
         Clock.schedule_once(self.dismiss_popup, self.dismiss_after)
 
 
